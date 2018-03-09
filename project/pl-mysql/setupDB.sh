15f9e97f29e915f5fe3b6d69e3fd5810f0a2279e
Cheryl L <clockett6@gatech.edu>
2018-03-09 01:17:37 -0600

    checkin working files

--- project/pl-mysql/setupDB.sh
+++ project/pl-mysql/setupDB.sh
@@ -9,17 +9,30 @@ mysql -u iwant -pmoreLiverPlease livermore -e "create table patients (
 	loinc varchar(20) DEFAULT NULL, 
 	PRIMARY KEY (id) );"
 
-# give user iwant access 
+mysql -u iwant -pmoreLiverPlease livermore -e "create table diseaseEducation (
+	id int(3) NOT NULL AUTO_INCREMENT,
+	loinc varchar(20) DEFAULT NULL, 
+	diseaseName varchar(30) DEFAULT NULL, 
+	diseaseDescription varchar(1000) DEFAULT NULL, 
+	diseaseTypicalCauses varchar(1000) DEFAULT NULL, 
+	diseaseTypicalSymptoms varchar(1000) DEFAULT NULL,
+	diseaseTypicalTreatment varchar(1000) DEFAULT NULL,
+	patientSymptoms varchar(1000) DEFAULT NULL,
+	patientTreatment varchar(1000) DEFAULT NULL,
+	PRIMARY KEY (id) );"
+#	not sure how useful this is, but anyhoo can be added FOREIGN KEY (loinc) REFERENCES patients(loinc),
+
+# give user iwant access from everywhere including host command line
 mysql -u root -pmysecretpassword livermore -e "grant all privileges on livermore.* to 'iwant'@'%' identified by 'moreLiverPlease';"
 
 #load data into database
 # need to create a compose volume to do this I think...
 #mysqlimport --fields-terminated-by=, --columns='id,firstName, lastName, patientId, loinc' --local -u root -p livermore /usr/local/share/livermore/patients.csv
 
-#mysql -u root -pmysecretpassword livermore -e "load data infile '/var/lib/liver/patients.txt' into table patients;"
+#mysql -u root -pmysecretpassword livermore -e "load data infile '/var/lib/mysql/patients.txt' into table patients;"
 
 mysql -u iwant -pmoreLiverPlease livermore -e "insert into patients
 	(id, firstName, lastName, patientId, loinc)
 	values
-	(1,'George', 'Burdell', '20185', '4789');"
+	(1,'GeorgeA', 'BurdellA', '20185A', '4789A');"
 
