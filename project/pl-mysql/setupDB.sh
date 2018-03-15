fd75b2e9b96cad704b22199ae3d78cc8c8faea56
Cheryl L <clockett6@gatech.edu>
2018-03-15 02:21:12 -0500

    Added volume support for mysql.  Now you can put files into pl-mysql directory and they show up in /usr/share/mysql-data on container.  You can edit files as needed without restarting everything.  If adding files, that may need to be restarted to load into database on startup.

--- project/pl-mysql/setupDB.sh
+++ project/pl-mysql/setupDB.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #create a table 
-mysql -u iwant -pmoreLiverPlease livermore -e "create table patients (
-	id int(3) NOT NULL AUTO_INCREMENT, 
+mysql -v -u iwant -pmoreLiverPlease livermore -e "create table patients (
+	id int NOT NULL AUTO_INCREMENT, 
 	firstName varchar(20) DEFAULT NULL, 
 	lastName varchar(20) DEFAULT NULL, 
 	patientId varchar(20) DEFAULT NULL, 
-	loinc varchar(20) DEFAULT NULL, 
+	snomed varchar(20) DEFAULT NULL, 
 	PRIMARY KEY (id) );"
 
-mysql -u iwant -pmoreLiverPlease livermore -e "create table diseaseEducation (
-	id int(3) NOT NULL AUTO_INCREMENT,
-	loinc varchar(20) DEFAULT NULL, 
+mysql -v -u iwant -pmoreLiverPlease livermore -e "create table diseaseEducation (
+	id int NOT NULL AUTO_INCREMENT,
+	snomed varchar(20) DEFAULT NULL, 
 	diseaseName varchar(30) DEFAULT NULL, 
 	diseaseDescription varchar(1000) DEFAULT NULL, 
 	diseaseTypicalCauses varchar(1000) DEFAULT NULL, 
@@ -20,19 +20,9 @@ mysql -u iwant -pmoreLiverPlease livermore -e "create table diseaseEducation (
 	patientSymptoms varchar(1000) DEFAULT NULL,
 	patientTreatment varchar(1000) DEFAULT NULL,
 	PRIMARY KEY (id) );"
-#	not sure how useful this is, but anyhoo can be added FOREIGN KEY (loinc) REFERENCES patients(loinc),
+#	not sure how useful this is, but anyhoo can be added FOREIGN KEY (snomed) REFERENCES patients(loinc),
 
-# give user iwant access from everywhere including host command line
-mysql -u root -pmysecretpassword livermore -e "grant all privileges on livermore.* to 'iwant'@'%' identified by 'moreLiverPlease';"
 
-#load data into database
-# need to create a compose volume to do this I think...
-#mysqlimport --fields-terminated-by=, --columns='id,firstName, lastName, patientId, loinc' --local -u root -p livermore /usr/local/share/livermore/patients.csv
+mysql -v -u root -pmysecretpassword livermore -e "load data infile '/usr/share/mysql-data/patients.txt' into table patients fields terminated by ',';"
 
-#mysql -u root -pmysecretpassword livermore -e "load data infile '/var/lib/mysql/patients.txt' into table patients;"
-
-mysql -u iwant -pmoreLiverPlease livermore -e "insert into patients
-	(id, firstName, lastName, patientId, loinc)
-	values
-	(1,'GeorgeA', 'BurdellA', '20185A', '4789A');"
 
