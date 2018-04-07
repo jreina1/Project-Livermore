900023e0a0bc5af7d9aa12eed54c8508f262739a
Andrew Lam <alam3@gatech.edu>
2018-04-07 14:32:26 -0500

    Fixed Infobar width issue; Made Infobar display by default; Changed infobar header-button to dark-grey

--- project/pl-webui/web/static/resources/js/interface.js
+++ project/pl-webui/web/static/resources/js/interface.js
@@ -1,9 +1,9 @@
 // Hide/Show the Disease Information panel
 function infoHide(id) {
 	var x = document.getElementById(id);
-	if (x.className.indexOf("w3-show") == -1) {
-		x.className += " w3-show";
+	if (x.className.indexOf("w3-hide") == -1) {
+		x.className += " w3-hide";
 	} else {
-		x.className = x.className.replace(" w3-show", "");
+		x.className = x.className.replace(" w3-hide", "");
 	}
 }
