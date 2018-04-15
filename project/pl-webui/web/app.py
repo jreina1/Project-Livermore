b6cc8f9636b5cd8feb8d64d27a26ee4377dc8033
Jennifer Reina <jreina3@gatech.edu>
2018-04-15 17:04:51 -0500

    app.py changes and new disease table query

--- project/pl-webui/web/app.py
+++ project/pl-webui/web/app.py
@@ -25,25 +25,14 @@ def update():
     Patient_diagnosis= ""
     Patient_symptoms= ""
     Patient_treatment= ""
-    Disease_name= ""
-    Disease_overview= ""
-    Disease_symptoms= ""
-    Disease_treatment= ""
-    Disease_causes= ""
-    Disease_risk_factors = ""
-    Disease_complications = ""
-    Disease_preventions = ""
-    Disease_resources = ""
+
 
     patient = request.form['patientId']
     if patient:   
         
-        query_patient = '''SELECT p.Patient_first_name, p.Patient_last_name, p.Patient_diagnosis, p.Patient_symptoms, p.Patient_treatment,
-                                d.Disease_name, d.Disease_overview, d.Disease_symptoms, d.Disease_treatment, d.Disease_causes, d.Disease_risk_factors, d.Disease_complications, d.Disease_preventions, d.Disease_resources
-                           FROM Patient p
-                           LEFT JOIN Disease d
-                           ON p.id = d.id
-                           WHERE p.Id = %s;''' %patient
+        query_patient = '''SELECT Patient_first_name, Patient_last_name, Patient_diagnosis, Patient_symptoms, Patient_treatment    
+                           FROM Patient
+                           WHERE Id = %s;''' %patient
         
         
         
@@ -57,15 +46,6 @@ def update():
             Patient_diagnosis= row[2]
             Patient_symptoms= row[3]
             Patient_treatment= row[4]
-            Disease_name= row[5]
-            Disease_overview= row[6]
-            Disease_symptoms= row[7]
-            Disease_treatment= row[8]
-            Disease_causes= row[9]
-            Disease_risk_factors = row[10]
-            Disease_complications = row[11]
-            Disease_preventions = row[12]
-            Disease_resources = row[13]
         db.close()
      
      
@@ -74,7 +54,57 @@ def update():
                     'Patient_last_name' : Patient_last_name,
                     'Patient_diagnosis' : Patient_diagnosis,
                     'Patient_symptoms' : Patient_symptoms,
-                    'Patient_treatment' : Patient_treatment,
+                    'Patient_treatment' : Patient_treatment})
+
+@app.route('/update_Disease', methods=['POST'])
+def update_Disease():
+     
+    db = MySQLdb.connect(host="pl-mysql",    # your host, usually localhost
+                     user="iwant",         # your username
+                     passwd="moreLiverPlease",  # your password
+                     db="livermore")        # name of the data base
+    
+
+    cur = db.cursor()
+
+
+    Disease_name= ""
+    Disease_overview= ""
+    Disease_symptoms= ""
+    Disease_treatment= ""
+    Disease_causes= ""
+    Disease_risk_factors = ""
+    Disease_complications = ""
+    Disease_preventions = ""
+    Disease_resources = ""
+
+    snomed = request.form['Patient_snomed_code']
+    if snomed:   
+        
+        query_snomed = '''SELECT Disease_name, Disease_overview, Disease_symptoms, Disease_treatment, Disease_causes, Disease_risk_factors, Disease_complications, Disease_preventions, Disease_resources
+                          FROM Disease
+                          WHERE Patient_snomed_code = %s;''' %snomed
+        
+        
+        
+        
+        cur.execute(query_snomed)
+
+        for row in cur.fetchall():
+            
+            Disease_name = row[0]
+            Disease_overview = row[1]
+            Disease_symptoms = row[2]
+            Disease_treatment = row[3]
+            Disease_causes = row[4]
+            Disease_risk_factors = row[5]
+            Disease_complications = row[6]
+            Disease_preventions = row[7]
+            Disease_resources = row[8]
+        db.close()
+     
+     
+    return jsonify({'result' : 'success', 
                     'Disease_name' : Disease_name,
                     'Disease_overview' : Disease_overview,
                     'Disease_symptoms' : Disease_symptoms,
