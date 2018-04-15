e24591ebf2c5ad3b832b029a385d1d80dbd182b0
Cheryl L <clockett6@gatech.edu>
2018-04-15 15:27:15 -0500

    reorder schema and data create

--- project/pl-mysql/setupDB.sh
+++ project/pl-mysql/setupDB.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mysql -v -u root -pmysecretpassword livermore < /docker-entrypoint-initdb.d/livermore_schema.sql
+
 mysql -v -u root -pmysecretpassword livermore -e "load data infile '/docker-entrypoint-initdb.d/patients.txt' into table PatientID fields terminated by ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n';"
 
 mysql -v -u root -pmysecretpassword livermore -e "LOAD DATA INFILE '/docker-entrypoint-initdb.d/DiseaseTable.txt' INTO TABLE Disease FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n';"
